99be24e3133e11e65a7add06717065f704976f17
0xf0000629 <xfutur85@gmail.com>
2025-05-21 22:47:17 +0300

    the functions!!!!!!

--- src/main/java/math/BaseFunctions.java
+++ src/main/java/math/BaseFunctions.java
@@ -1,4 +1,37 @@
 package math;
 
 public class BaseFunctions {
+    static final double PI = 3.14159265358979323846;
+    static double sin(double x) {
+        double mid = x, sum = x;
+        int f = 1;
+        for (int i = 0; i < 100; i++) {
+            f += 2;
+            mid = -mid * x * x / f / (f - 1);
+            sum += mid;
+        }
+        return sum;
+    }
+    static double cos(double x){
+        return sin(PI / 2 - x);
+    }
+    static double csc(double x){
+        return 1/sin(x);
+    }
+    static double sec(double x){
+        return 1/cos(x);
+    }
+    static double ln(double x){
+        if (x <= 0) return -1; // idk throw something
+        double y = (x - 1) / (x + 1);
+        double y2 = y * y, res = 0, pow = y;
+        for (int n = 0; n < 100; ++n) {
+            res += pow / (2 * n + 1);
+            pow *= y2;
+        }
+        return 2 * res;
+    }
+    static double log(double l, double x){
+        return ln(x) / ln(l);
+    }
 }
