0fa16e593d0ddbde1cd3fd5c4b22dafd9a9f7d96
user1 <pozdov.ivan@yandex.ru>
2025-05-21 23:24:36 +0300

    nothing

--- src/main/java/math/BaseFunctions.java
+++ src/main/java/math/BaseFunctions.java
@@ -2,6 +2,7 @@ package math;
 
 public class BaseFunctions {
     static final double PI = 3.14159265358979323846;
+
     static double sin(double x) {
         double mid = x, sum = x;
         int f = 1;
@@ -12,16 +13,20 @@ public class BaseFunctions {
         }
         return sum;
     }
-    static double cos(double x){
+
+    static double cos(double x) {
         return sin(PI / 2 - x);
     }
-    static double csc(double x){
-        return 1/sin(x);
+
+    static double csc(double x) {
+        return 1 / sin(x);
     }
-    static double sec(double x){
-        return 1/cos(x);
+
+    static double sec(double x) {
+        return 1 / cos(x);
     }
-    static double ln(double x){
+
+    static double ln(double x) {
         if (x <= 0) return -1; // idk throw something
         double y = (x - 1) / (x + 1);
         double y2 = y * y, res = 0, pow = y;
@@ -31,7 +36,14 @@ public class BaseFunctions {
         }
         return 2 * res;
     }
-    static double log(double l, double x){
+
+    static double log(double l, double x) {
         return ln(x) / ln(l);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("real ln(0.00001): " + Math.log(0.00001));
+        System.out.println("our ln(0.00001): " + ln(0.00001));
+
     }
 }
